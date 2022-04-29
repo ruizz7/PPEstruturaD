1e81099c888a2174a2e8b089d36fd91354c5e9ef
ruizz7 <ruizigor777@gmail.com>
2022-04-29 03:11:53 -0300

    questao1att

---
+++ src/exercicio1/Lista.java
@@ -0,0 +1,55 @@
+package exercicio1;
+
+public class Lista {
+	No inicio, fim;
+	
+	// insere um n� no final da lista. Esse m�todo ser� chamado pelo m�todo inserir
+	private void inserirFim(No aux) {
+		if(inicio == null) {
+			inicio = aux;
+		} else {
+			fim.prox = aux;
+		}
+		fim = aux;
+	}
+	
+	//esse m�todo dever� ser implementado de acordo com o enunciado do exerc�cio
+	private void inserirPrioridade(No aux) {
+		if(inicio == null) {
+			inicio = aux;
+		} else {
+			No aux_f = inicio;
+			while(aux_f.prox.cor == aux.cor){
+				aux_f = aux_f.prox;
+			}
+			if (aux_f.cor != aux.cor) {
+				inicio = aux;
+				aux.prox = aux_f;
+			} else {
+				No prox_ultimo = aux_f.prox;
+				aux_f.prox = aux;
+				aux.prox = prox_ultimo;
+			}
+		}
+	}
+	
+	
+	// m�todo inserir. Esse m�todo ser� chamado a partir da classe Main
+	public void inserir(int numero, String cor) {
+		No aux = new No(numero, cor);
+		if(cor.equalsIgnoreCase("verde")) {
+			inserirFim(aux);
+		} else {
+			inserirPrioridade(aux);
+		}
+	}
+	
+	// m�todo utilizado para imprimir os dados da lista
+	public void imprimir() {
+		No aux = inicio;
+		while(aux != null) {
+			System.out.println(aux.numero);
+			aux = aux.prox;
+		}
+	}
+}
